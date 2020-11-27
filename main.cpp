b3c594b591c60f0be2a0c1778287b56e1f71ed86
krubnjos <71826256+JosefKrubner@users.noreply.github.com>
2020-11-27 12:15:41 +0100

    Update main.cpp
    Kosmetické úpravy pro zpřehlednění kódu.

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-		//Nastaveni barev konzole
-			system("Color B0");
-	
+	//Nastaveni barev konzole
+		system("Color B0");
 	//------------------------------------------------------------------------//
 		//Deklarace a inicializace pozivanych promennych
 			bool LED=0;				//Led vypnuto/zapnuto
@@ -18,61 +17,39 @@ int main(int argc, char** argv) {
 			float T=25.45;			//Nastaveni teploty
 			float ventProc=56.; 	//Nastaveni vykonu ventilatoru
 			short int modVent=1;	//Nastaveni modu chodu ventilatoru
-									// 0-vypnuto	1-auto	2-manual
+						// 0-vypnuto	1-auto	2-manual
 			short int modT=1;		//Nastaveni modu nastaveni teploty
-									// 0-manualni	1-nahodna	tbd.:(2,3 - linearni, harmonicky)
-			
+							// 0-manualni	1-nahodna	tbd.:(2,3 - linearni, harmonicky)
 	//------------------------------------------------------------------------//
 		//Inicializace pocitani casu = prostredek pro nahodne teploty
-		
 			srand(time(NULL));		
-
 	//------------------------------------------------------------------------//
 		//Hlavni smycka programu
-		
 			while(true){
-				
 				//------------------------------------------------------------//
 					//Pri nastavenem nahodnem generovani teploty vygeneruje teplotu
-					
 						if (modT == 1){
 							T = fNahodnaTeplota();
 						}
-						
 				//------------------------------------------------------------//
-					//Vypsani promennych do input souboru pro hlavni program
-					
-						fVypisDoInputSouboru(T, LED, modVent, ventProc);
-						
+					//Vypsani promennych do input souboru pro hlavni program					
+						fVypisDoInputSouboru(T, LED, modVent, ventProc);						
 				//------------------------------------------------------------//
-					//Vycisteni konzole
-					
-						system("cls"); 
-						
-				
-						
+					//Vycisteni konzole					
+						system("cls"); 																
 				//------------------------------------------------------------//
-					//Vypis uvodniho textu a stavove tabulky
-					
+					//Vypis uvodniho textu a stavove tabulky					
 						fUvodniVypis();
-						fStavovaTabulka(LED, modVent, ventProc, modT, T);
-				
+						fStavovaTabulka(LED, modVent, ventProc, modT, T);				
 				//Volani hlavni nabidky	
-					fNabidka(LED, konec, T, ventProc, modVent, modT);		
-				
-					// Podminka pro vyskoceni z programu
-						
+					fNabidka(LED, konec, T, ventProc, modVent, modT);						
+					// Podminka pro vyskoceni z programu						
 						if (konec == 1) {
-							break;}
-							
-			}//Konec hlavni smycky programu	
-		
-	//------------------------------------------------------------------------//	
-		
-		//Cekani na klavesu (pri spouseni mimo IDE)
-		
+							break;}							
+			}//Konec hlavni smycky programu			
+	//------------------------------------------------------------------------//			
+		//Cekani na klavesu (pri spouseni mimo IDE)		
 		cout << "Program ukoncen. Stiskni libovolnou klavesu...";
-		getch();
-					
-		return 0;
+		getch();					
+	return 0;
 }

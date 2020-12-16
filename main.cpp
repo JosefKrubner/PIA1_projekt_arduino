ad234c7de4c251cdf4404041453d3c462ce71ab0
krubnjos <71826256+JosefKrubner@users.noreply.github.com>
2020-12-16 10:19:39 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,39 +10,44 @@ Program informuje o èinnosti LED, pøekroèení povolené teploty, aktuální teplotì,
 using namespace std;
 
 int main(){
-	
+	//system("Color B0");
 	/*----------------Deklarace promennych---------------*/
 	float T, vent, nVent;
 	short int modVent, procVent, LED;
 	/*---------------------------------------------------*/
 	
-	/*------------Nacteni hodnot z programu 2--------------*/
-	fCteniZOutputSouboru(LED, T, vent, nVent, modVent);					
-	/*-----------------------------------------------------*/
-	
-	
-	/*----------------Vypis na obrazovku-------------------*/
-	
-	//Teplota
-		cout <<"Teplota tisku je " << T << " stupnu C" << endl;
-	
-	//LED zapnuto/vypnuto
-	if(LED == 0){
-		cout <<"LED osvetleni je vypnuto." << endl;
-	} else if(LED == 1){
-		cout <<"LED osvetleni je zapnuto." << endl;
-	} else{
-		cout <<"Byla prekrocena povolena teplota tisku!" << endl;
-	};
-	
-	//mod a mira chodu ventilatoru
-	if(modVent == 0){
-		cout << "Ventilator je vypnuty." << endl;
-	} else if(modVent == 1){
-		cout << "Ventiltor pracuje v automatickem rezimu s aktualnim napetim " << nVent << " V." << endl;
-	} else{
-		cout << "Ventilator pracuje v manualnim rezimu s aktualnim napetim " << nVent << " V." << endl;
-	};
+	while(true){
+		system("cls");
+		/*------------Nacteni hodnot z programu 2--------------*/
+		fCteniZOutputSouboru(LED, T, vent, nVent, modVent);					
+		/*-----------------------------------------------------*/
+		
+		
+		/*----------------Vypis na obrazovku-------------------*/
+		
+		//Teplota
+			cout <<"Teplota tisku je " << T << " stupnu C" << endl;
+		
+		//LED zapnuto/vypnuto
+		if(LED == 0){
+			cout <<"LED osvetleni je vypnuto." << endl;
+		} else if(LED == 5){
+			cout <<"LED osvetleni je zapnuto." << endl;
+		} else{
+			cout <<"Byla prekrocena povolena teplota tisku!" << endl;
+		};
+		
+		//mod a mira chodu ventilatoru
+		if(modVent == 0){
+			cout << "Ventilator je vypnuty." << endl;
+		} else if(modVent == 1){
+			cout << "Ventilator pracuje v automatickem rezimu s aktualnim napetim " << nVent << " V." << endl;
+		} else{
+			cout << "Ventilator pracuje v manualnim rezimu s aktualnim napetim " << nVent << " V." << endl;
+		};
+		//if(getch() == 27) break;
+		sleep(2);
+	}
 	
 	
 	/*-----------------------------------------------------*/
